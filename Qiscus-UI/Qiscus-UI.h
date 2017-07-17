efe68d10df44883ad15b722bbd792e1b3a54e465
asharijuang <asharijuang@me.com>
2017-07-17 11:39:52 +0700

    initiate project

---
+++ Qiscus-UI/Qiscus-UI.h
@@ -0,0 +1,19 @@
+//
+//  Qiscus-UI.h
+//  Qiscus-UI
+//
+//  Created by asharijuang on 7/17/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Qiscus-UI.
+FOUNDATION_EXPORT double Qiscus_UIVersionNumber;
+
+//! Project version string for Qiscus-UI.
+FOUNDATION_EXPORT const unsigned char Qiscus_UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Qiscus_UI/PublicHeader.h>
+
+
